ec713110ea35972cc8da2f2690cbaedfd3a9b617
kelly <kkelly.luo@gmail.Com>
2024-12-09 08:36:03 +0100

    Question 2 en mieux avec commentaire en anglais

--- enseash.c
+++ enseash.c
@@ -9,15 +9,15 @@
 
 /* void displayTxtConsole(char *path)
  * 
- * prend en argument le chemin du fichier à afficher 
- * et afficher le contenu du fichier dans la console standard output 
+ * take in argument the path of the file to display 
+ * displays the content of the file in the console standard output
  */
 void displayTxtConsole(char *path)
 {
 	int fd; ssize_t ret; 
 	char buf[BUFSIZE];
 	
-	// on ouvre le fichier txt en lecture seule, on reccupere le file desriptor 
+	// open the .txt file in read only and get the file descriptor
 	fd = open(path, O_RDONLY);
 	if (fd == 0) 
 	{
@@ -25,7 +25,7 @@ void displayTxtConsole(char *path)
 		exit(EXIT_FAILURE);
 	}
 	
-	// on lit le fichier qu'on a ouvert et on l'écrit dans la console
+	// read the file et write the content stored in buf in the console
 	while((ret = read(fd, buf, BUFSIZE))>0) {
 		if(write(STDOUT_FILENO, buf, ret) == -1) {
 			perror("error in write");
@@ -33,21 +33,22 @@ void displayTxtConsole(char *path)
 		}
 	}
 	
+	// if error in reading 
 	if (ret == -1){ perror("error in read");}
 	close(fd);
 }
 
 
-/* string readConsole(void)
+/* char *readConsole(void)
  * 
- * lit ce qui est écrit dans la console et le retourne en string 
+ * read what is written in the console and return it with a char pointer
  */
 char *readConsole(void) 
 {
 	ssize_t ret; 
 	char *buf = malloc(BUFSIZE);
 	
-	// on lit la console et on met le truc dans la variable buf
+	// what is read is in buf
 	ret = read(STDIN_FILENO, buf, BUFSIZE - 1);
 
 	if (ret == -1){ perror("error in read");}
@@ -55,49 +56,54 @@ char *readConsole(void)
 	return buf;
 }
 
+/* void ifExit(char * command)
+ * 
+ * management of the shell exit 
+ */
+void ifExit(char * command) 
+{
+	 if (strcmp(command, "exit") == 0) {
+		displayTxtConsole("byebye.txt");
+		exit(EXIT_SUCCESS);
+	}
+}
+
 int main (int argc, char *argv[]) 
 {
-	
+	int status; 
+	// displays welcome message 
 	displayTxtConsole("welcome.txt");
 	
 	while(1) 
 	{
 		displayTxtConsole("prompt.txt");
 		char *command = readConsole();
-		command[strcspn(command, "\n")] = 0;	// supprime le retourn a la ling \n à la fin de la commande 
+		command[strcspn(command, "\n")] = 0;	// delete the \n at the end of the command
 		
-		// gestion du exit 
-		 if (strcmp(command, "exit") == 0) {
-            displayTxtConsole("byebye.txt");
-            exit(EXIT_SUCCESS);
-        }
+		ifExit(command);
 		
-		// fork et on verifie qu'il n'y a pas d'erreur
+		// fork and we check there is no error 
 		pid_t pid = fork();
 		if(pid < 0){
-			perror("Erreur lors du fork");
+			perror("Erreur forking");
 			return EXIT_FAILURE;
 		}
 		
-		// le fork enfant execute la commande 
+		// the fork children execute the command with
 		if (pid == 0) {
-			execvp(command, (char *[]) {command, NULL}); // on execute la commande en ajoute le NULL à la fin de la chaine
-			perror("Erreur lors de l'exécution de la commande");
+			execvp(command, (char *[]) {command, NULL}); // children execute command 
+			perror("Erreur when executing command");
 			exit(EXIT_FAILURE);
 		}
-		// le fork parent attend au cas ou son enfant se fait tué 
+		// the fork parent wait in case something wrong happens to his children
 		else 
 		{
-			int status; 
 			wait(&status);
 			if (!WIFEXITED(status)) 
 			{
 				exit(EXIT_FAILURE);
 			}
-		}
-		
+		}	
 	}
-	
-	
 }
 
